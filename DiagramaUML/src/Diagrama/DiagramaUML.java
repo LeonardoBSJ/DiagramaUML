f0a8044d1ee705dafeb916f19d1c609811cef336
LeonardoBSJ <126364192+LeonardoBSJ@users.noreply.github.com>
2023-09-09 19:11:00 -0600

    Update DiagramaUML.java

--- DiagramaUML/src/Diagrama/DiagramaUML.java
+++ DiagramaUML/src/Diagrama/DiagramaUML.java
@@ -5,23 +5,23 @@ import java.util.ArrayList;
 public class DiagramaUML {
 
     public static void main(String[] args) {
-        Futbolista futbolista1 = new Futbolista("Chivas") {};
+        Futbolista futbolista1 = new Futbolista(" Chivas ") {};
         futbolista1.setCantidadIntegrantes(18);
-        futbolista1.setNombre("Juan");
+        futbolista1.setNombre(" Juan ");
         futbolista1.setNumPlayera(17);
-        futbolista1.setPosicion("Delantero");
+        futbolista1.setPosicion(" Delantero ");
 
-        Futbolista futbolista2 = new Futbolista("America");
+        Futbolista futbolista2 = new Futbolista(" América ");
         futbolista2.setCantidadIntegrantes(19);
-        futbolista2.setNombre("max");
+        futbolista2.setNombre(" max ");
         futbolista2.setNumPlayera(17);
-        futbolista2.setPosicion("Defenza");
+        futbolista2.setPosicion(" Defenza ");
          
-        Futbolista futbolista3 = new Futbolista("pumas");
+        Futbolista futbolista3 = new Futbolista(" pumas ");
         futbolista3.setCantidadIntegrantes(18);
-        futbolista3.setNombre("juan polito");
+        futbolista3.setNombre(" Hugo ");
         futbolista3.setNumPlayera(7);
-        futbolista3.setPosicion("medio");
+        futbolista3.setPosicion(" medio ");
         
         SeleccionFutbolista seleccion1 = new SeleccionFutbolista(4," Mexico ",futbolista1);
         seleccion1.agregar(futbolista2);
